3b66e990ea5f51037600ffa72c49f7fb12dd83eb
amikatzoty <103873311+amikatzoty@users.noreply.github.com>
2023-01-31 13:46:44 +0200

    Update UnitTest2.cs

--- classProject/Tests/UnitTest2.cs
+++ classProject/Tests/UnitTest2.cs
@@ -1,57 +1,95 @@
-﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
 using AmazonProj;
-using NUnit.Framework;
-using classProject.Utils;
+using System.IO;
+using AngleSharp.Dom;
 
-namespace classProject.Tests
+namespace AmazonProj
 {
-    public class UnitTest2 : BrowserFactory
+    public class Results
     {
-
-        List<IWebElement> mouses = new List<IWebElement>();
-        new Dictionary<string, string> itemfilter = new Dictionary<string, string>();
-
-        //Dictionary<string, string> filterDictionary = new Dictionary<string, string>();
-      //  List<IWebElement> elementsList;
-      //  List<Item> itemsList;
-
-        [Test]
-        public void test()
+        private IWebDriver driver;
+        
+        
+        private string xpath = "//span[@class='a-price-whole']";
+        
+        private Dictionary<string, string> filter = new Dictionary<string, string>();
+        
+        public Dictionary<string, string> Filter
         {
-            Amazon Amazon = new Amazon(driver);
-            driver.Navigate().GoToUrl("https://www.amazon.com");
+            get { return this.filter; }
+            set { this.filter = value; }
+        }
+        public Results(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        enum DictionaryKeys
+        {
+            price_lower_then,
+            price_higher_or_equal,
+            free_shipping
+        }
 
-            Amazon.Pages.Home.SearchBar.Text = "mouse";
-            Amazon.Pages.Home.SearchBar.Click();
-            //Amazon.Pages.Home.SearchBar.Text = "mouse";
-            //Amazon.Pages.Home.SearchBar.Click();
-
-            //creat a result list after filltering
-            List<Item> items = Amazon.Pages.Results.GetResultBy(new Dictionary<string, string>() {
-                { "Price_Lower_Then", "100" },{"Price_Hiegher_OR_Equal_Then", "10" },{"Free_Shipping", "true"}});
-
-            foreach (Item item in items)
+        public List<Item> GetResultBy(Dictionary<string, string> filter)
+        {
+            IList<IWebElement> elements = new List<IWebElement>(); 
+            //preform our fillter search
+            foreach (var filterxpath in filter)
             {
-                Console.WriteLine(item.Title);
-                Console.WriteLine(item.Price + "\n");
-                //Console.WriteLine(item.Url);
+                switch (filterxpath.Key)
+                {
+                    case "Price_Lower_Then":
+                        
+                        xpath += "[text()<" + filterxpath.Value + "]";
+                        break;
+                    case "Price_Hiegher_OR_Equal_Then":
+                        xpath += "[text()>=" + filterxpath.Value + "]";
+                        break;
+                    case "Free_Shipping":
+
+                        xpath += "/../../../../../../../..//span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]";
+
+
+                        break;
+                }
             }
-            Assert.Pass();
-            //Amazon.Pages.Results.GetResultsBy();
+            xpath += "/../../../../../..";
+            elements = driver.FindElements(By.XPath(xpath));
+            //correct xpath = //span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]/../../../../..//span[@class='a-price-whole'][text()>='10' and text() <100]/../../../../../../../../..
+            //another corret xpath = //span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]/../../../../..//span[@class='a-price-whole'][text()>='10'][text() <100]/../../../../../../../../..
+            //best xpath = //span[@class='a-price-whole'][text()>='10'][text() <100]/../../../../../../../..//span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]
+
+
+            List<Item> items = new List<Item>();
+            for(int i = 0; i < elements.Count; i++) 
+            {
+                
+
+                String name = elements[i].FindElement(By.XPath(".//span[@class='a-size-medium a-color-base a-text-normal']")).Text;
+                String price = elements[i].FindElement(By.XPath(".//span[@class='a-price-whole']")).Text + "." + elements[i].FindElement(By.XPath(".//span[@class='a-price-fraction']")).Text + "$";
+                String shipping = "Free Shipment";
+                
+                 Item item = new Item(name,price, shipping);
+                 items.Add(item);
+            }
+
+            return items;
+
+
+           
+
+            
         }
-        [TearDown]
-        public void closeBrowser()
-        {
-            //driver.Quit();
-        }
+
+
+
+
     }
 }
