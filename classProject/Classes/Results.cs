516fb79a1cfa86e7fef43c3a08733a299fdbed0b
amikatzoty <103873311+amikatzoty@users.noreply.github.com>
2023-01-31 13:43:48 +0200

    Update Results.cs

--- classProject/Classes/Results.cs
+++ classProject/Classes/Results.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using AmazonProj;
 using System.IO;
+using AngleSharp.Dom;
 
 namespace AmazonProj
 {
     public class Results
     {
         private IWebDriver driver;
-        //IReadOnlyList<IWebElement> items;
-        private string xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small'";
-        //IList<IWebElement> items;
+        
+        
+        private string xpath = "//span[@class='a-price-whole']";
+        
         private Dictionary<string, string> filter = new Dictionary<string, string>();
-        //private string xpath = "$x("//span[@class='a-price' and translate(descendant::span[@class='a-offscreen']//.,'$','')]")";
+        
         public Dictionary<string, string> Filter
         {
             get { return this.filter; }
@@ -37,60 +39,57 @@ namespace AmazonProj
 
         public List<Item> GetResultBy(Dictionary<string, string> filter)
         {
+            IList<IWebElement> elements = new List<IWebElement>(); 
             //preform our fillter search
             foreach (var filterxpath in filter)
             {
                 switch (filterxpath.Key)
                 {
                     case "Price_Lower_Then":
-                        xpath += "and concat(concat(descendant::span[@class='a-price-whole'], descendant::span[class='a-price-decimal']), descendant::span[@class='a-price-fraction']) <" + filterxpath.Value;
+                        
+                        xpath += "[text()<" + filterxpath.Value + "]";
                         break;
                     case "Price_Hiegher_OR_Equal_Then":
-                        xpath += "and concat(concat(descendant::span[@class='a-price-whole'], descendant::span[class='a-price-decimal']),descendant::span[@class='a-price-fraction']) >=" + filterxpath.Value;
+                        xpath += "[text()>=" + filterxpath.Value + "]";
                         break;
                     case "Free_Shipping":
-                        if (filterxpath.Value == "true") xpath += " and descendant::span[contains(text(), 'FREE')]";
+
+                        xpath += "/../../../../../../../..//span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]";
+
+
                         break;
                 }
             }
-            xpath += "]";
+            xpath += "/../../../../../..";
+            elements = driver.FindElements(By.XPath(xpath));
+            //correct xpath = //span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]/../../../../..//span[@class='a-price-whole'][text()>='10' and text() <100]/../../../../../../../../..
+            //another corret xpath = //span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]/../../../../..//span[@class='a-price-whole'][text()>='10'][text() <100]/../../../../../../../../..
+            //best xpath = //span[@class='a-price-whole'][text()>='10'][text() <100]/../../../../../../../..//span[contains(text(),'משלוח בחינם') or contains(text(),'משלוח חינם')]
 
 
-            List<IWebElement> elements = driver.FindElements(By.XPath(xpath)).ToList();
             List<Item> items = new List<Item>();
-            //string url = elements[0].FindElement(By.XPath((".//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']"))).GetAttribute("href");
-            foreach (IWebElement element in elements)
+            for(int i = 0; i < elements.Count; i++) 
             {
-                //the the title and the price and add it to the list
-                string title = element.FindElement(By.XPath((".//span[@class='a-size-medium a-color-base a-text-normal']"))).Text;
-                string price = element.FindElement(By.XPath(".//span[@class='a-price-whole']")).Text + '.' + element.FindElement(By.CssSelector(".a-price-fraction")).Text + '$';
-                string url = element.FindElement(By.XPath((".//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']"))).GetAttribute("href");
-                items.Add(new Item(title, price, url));
+                
+
+                String name = elements[i].FindElement(By.XPath(".//span[@class='a-size-medium a-color-base a-text-normal']")).Text;
+                String price = elements[i].FindElement(By.XPath(".//span[@class='a-price-whole']")).Text + "." + elements[i].FindElement(By.XPath(".//span[@class='a-price-fraction']")).Text + "$";
+                String shipping = "Free Shipment";
+                
+                 Item item = new Item(name,price, shipping);
+                 items.Add(item);
             }
+
             return items;
+
+
+           
+
+            
         }
 
 
 
 
-        /*  public List<Item> GetResultsBy(Dictionary<string,string>filters )
-          {
-              string xpath = "$x(\"//span[@class='a-price' and translate(descendant::span[@class='a-offscreen']//.,'$','')]\")";
-              foreach (KeyValuePair<string,string> filter in filters)
-              {
-                  switch (filter.Key)
-                  {
-                      case DictionaryKeys.price_lower_then.ToString():
-                          xpath = filter.Value;
-                          break;
-                      case DictionaryKeys.price_higher_or_equal.ToString():
-                          xpath = filter.Value;
-                          break;
-                      case DictionaryKeys.free_shipping.ToString():
-                          xpath = filter.Value;
-                          break;
-                  }
-              }
-          }*/
     }
 }
